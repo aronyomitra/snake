006fd82a41a661df2e08ba9bb443f51073f60656
Aronyo Mitra <aronyomitra@gmail.com>
2019-03-11 02:00:50 +0530

    Add food drops
    Snake gets bigger on eating
    Renamed scaling variable to scaling_factor

--- docs/todo.md
+++ docs/todo.md
@@ -4,7 +4,8 @@
 - Implement movement and redrawing (done)
 - Deal with long snake (done)
 
-- Create food drops
-- Make snake grow longer by eating
+- Create food drops (done)
+- Make snake grow longer by eating (done)
+
 - Collision detection and loss condition
 - Add walls to map (?)
---
+++ js/food.js
@@ -0,0 +1,23 @@
+function Food()
+{
+  this.pos = [];
+  this.isEaten = false;
+  this.score = 10;
+
+  this.initialize = function()
+  {
+    // Using the function i defined in my utilities library
+    // I want the coordinates of food to be multiples of 10 (scaling_factor)
+    // Hence, random(1, x)*10 will be a multiple of ten between 10 and x*10
+    this.pos[0] = utilities.random(1, canvas_size[0]/scaling_factor - 1);
+    this.pos[1] = utilities.random(1, canvas_size[1]/scaling_factor - 1);
+
+    this.pos[0] *= scaling_factor; this.pos[1] *= scaling_factor;
+  }
+
+  this.draw = function()
+  {
+    rectMode(CENTER);
+    rect(this.pos[0], this.pos[1], scaling_factor, scaling_factor);
+  }
+}
